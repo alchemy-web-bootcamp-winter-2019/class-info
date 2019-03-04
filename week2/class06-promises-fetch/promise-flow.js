52958b643764858090ba39abc1bf7e4896c9bf18
Willem Larsen <willemlarsen@gmail.com>
2019-03-04 10:04:40 -0800

    add console.logs and instructions

--- week2/class06-promises-fetch/promise-flow.js
+++ week2/class06-promises-fetch/promise-flow.js
@@ -1,14 +1,23 @@
-  fetch('https://swapi.co/api/people/1')
+throw new Error('Hi! I bombed your code'); 
+
+// have everyone copy and paste below into their chrome js console. Run. See result. Then:
+// Replace topmost comment with a thrown error (line 1). See result.
+// Replace next comment, see result, etc. Stop before catch on 23.
+
+fetch('https://swapi.co/api/people/1')
   .then((swData) => {
     // this only runs if there is no error getting user data
+    console.log('Made it back from the web!');
     return swData;
   })
   .then((swData) => {
     // this only runs if there is no error in all previous calls
+    console.log('Turn that stuff into json');
     return swData.json();
   })
   .then((swDataJson) => {
     // this again only runs if there is no error in all previous calls
+    console.log('Print out the name');
     console.log(swDataJson.name);
   })
   .catch((error) => {
