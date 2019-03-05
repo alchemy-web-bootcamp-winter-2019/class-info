dd1d5481b404fd8bd92c96dd17b5b4624e6da672
Willem Larsen <willem@alchemycodelab.com>
2019-03-05 10:15:15 -0800

    add map code cooking

---
+++ week2/class-08-map-promise-chaining/map-transform-code-cooking.js
@@ -0,0 +1,83 @@
+// MAP
+
+// don't code cook the arrays below; let everyone build 
+// these things and keep them
+
+const phrasebook = [
+    {
+        greeting: "Grüss dich",
+        language: 'German'
+    },
+    {
+        greeting: "Dia duit",
+        language: 'Irish',
+        response: "Dia s'mura dhit"
+    },
+    {
+        greeting: 'Marhaba',
+        language: 'Arabic',
+        response: 'Marhabteen'
+    },
+    
+];
+
+var numbers = [1, 4, 9];
+
+var words = ['french', 'fancy', 'fingerfood'];
+
+// once we've made these now proceed to code cook:
+
+
+numbers;
+
+
+
+numbers.map(number => number * 3);
+
+//
+
+words;
+
+//
+
+words.map(word => `helloooo my ${word} friend!`);
+
+//
+
+words.map(word => word.slice(1, 4));
+
+//
+
+phrasebook;
+
+//
+
+phrasebook.map(phrase => ({ greeting: phrase.greeting }));
+
+//
+
+phrasebook;
+
+//
+
+phrasebook.map(phrase => ({ greeting: phrase.greeting, response: phrase.response || 'no special response'}));
+
+//
+
+numbers;
+
+//
+
+numbers.map((number, index) => `${number} at the index of ${index}`);
+
+//
+
+words;
+
+//
+
+words.map((word, index) => `${word} at the index of ${index}`);
+
+
+
+
