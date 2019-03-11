bc4f6983d3d4e1f1292e1cefa03f60104f4395c4
Willem Larsen <willem@alchemycodelab.com>
2019-03-11 15:24:02 -0700

    add object keys code cook script

---
+++ week3/object-keys-code-cooking.js
@@ -0,0 +1,48 @@
+// OBJECT.KEYS
+
+// don't code cook the string, array and objects below; 
+// type these up and let everyone build 
+
+const fooString = 'foo';
+
+const fooArray = ['f', 'o', 'o'];
+
+const fooObject = {
+    1: 'f',
+    2: 'o',
+    3: 'o'
+};
+
+const phrasebook = {
+    greeting: "hello",
+    farewell: "good bye",
+    greetingTwo: "howdy"
+};
+
+// Now code cook!
+
+fooString;
+
+Object.keys(fooString);
+
+// 
+
+fooArray;
+
+Object.keys(fooArray);
+
+// 
+
+fooObject;
+
+Object.keys(fooObject);
+
+//
+
+phrasebook;
+
+JSON.stringify(phrasebook);
+
+Object.keys(phrasebook);
+
+
